e3bb31e87846d9c4c3d92dbf7d4a8a526fad6872
shrle <puyoquerensim@gmail.com>
2023-02-19 18:11:42 +0900

    add axios.create baseUrl

--- src/main.js
+++ src/main.js
@@ -5,7 +5,16 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const app = createApp(App);
-app.config.globalProperties.$http = axios;
+
+app.config.globalProperties.$http = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  responseType: "json",
+  timeout: 3000,
+});
+
 app.config.globalProperties.$userData = reactive({
   name: "",
 });
